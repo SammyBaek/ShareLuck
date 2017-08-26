f73c3a9044131242af0596763be580ffdbf71e56
Sam Baek <kie827@gmail.com>
2017-08-26 17:14:12 -0400

    Add a sample tool hardcoded to app.component

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+    tool = {
+        content: 'hammer',
+        owner: 'Sam 30062'
+    }
 }
---
+++ assets/app/tools/tool.model.ts
@@ -0,0 +1,9 @@
+export class Tool {
+    constructor(
+        public content: string,
+        public owner: string,
+        public toolId?: string,
+        public userId?: string
+
+) {}
+}
